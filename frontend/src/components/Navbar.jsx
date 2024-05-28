6d48744e71d3e7fc49507f2f8593e42ae1af987d
WendelEfraim <wendel.efrain11@gmail.com>
2024-05-28 17:01:47 -0300

    css modificado com logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom"
 import styles from "./navbar.module.css"
+import Logo from "../assets/img/pata.png"
 
 const Navbar = () =>{
     return(
         <nav className={styles.Navbar}>
+            <div className={styles.logo_Navbar}>
+                <img src={Logo} alt="My Pet" />
+                <h2>My pet</h2>
+            </div>
+
+            <div className={styles.linkNavbar}>
             <Link className ={styles.link} to="/">Home</Link>
             <Link className ={styles.link} to="/login">Login</Link>
             <Link className ={styles.link} to="/register">Register</Link>
+            </div>
         </nav>
     )
 }
