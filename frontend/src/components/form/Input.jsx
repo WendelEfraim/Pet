f1cf94a8e350e6056e1b46d44f213b9404e68ce7
WendelEfraim <wendel.efrain11@gmail.com>
2024-06-04 18:37:41 -0300

    estilização de container e navbar

--- frontend/src/components/form/Input.jsx
+++ frontend/src/components/form/Input.jsx
@@ -1,3 +1,5 @@
+import style from './Input.module.css'
+
 function Input ({
     type,
     text,
@@ -8,7 +10,7 @@ function Input ({
     multiple,
     }){
         return(
-            <div>
+            <div className={style.form_control}>
                 <label htmlFor={name}>{text}:</label>
                 <input type={type}
                 name={name}
